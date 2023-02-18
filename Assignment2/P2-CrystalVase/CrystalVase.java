8346c5b3ae845dfa4fd1f962b4b9f0b45801161d
ElijahMSmith <mcditto99@gmail.com>
2023-02-18 16:04:00 -0500

    HW2 finished

--- Assignment2/P2-CrystalVase/CrystalVase.java
+++ Assignment2/P2-CrystalVase/CrystalVase.java
@@ -1,5 +1,60 @@
-public class CrystalVase {
-    public static void main(String[] args) {
+import java.util.LinkedList;
+import java.util.Queue;
+import java.util.Scanner;
+import java.util.concurrent.ExecutorService;
+import java.util.concurrent.Executors;
+import java.util.concurrent.TimeUnit;
 
+public class CrystalVase {
+    public static void main(String[] args) throws InterruptedException {
+        /*
+         * Create all guest threads
+         * Wait for a second to let them queue up
+         * Start the first guest in the queue
+         * Wait for all threads to have stopped (executor pool again?)
+         */
+
+        int N = 0;
+        Scanner sc = new Scanner(System.in);
+        System.out.println("How many guests?");
+        while (true) {
+            try {
+                N = sc.nextInt();
+                if (N > 1)
+                    break;
+            } catch (Exception e) {
+                System.out.println("Please provide a valid number of guests (>= 1).");
+            }
+        }
+        sc.close();
+
+        Queue<GuestThread> vaseRoom = new LinkedList<GuestThread>();
+        ExecutorService es = Executors.newCachedThreadPool();
+
+        long start = System.currentTimeMillis();
+        GuestThread[] allGuests = new GuestThread[N];
+        for (int i = 0; i < N; i++) {
+            allGuests[i] = new GuestThread(vaseRoom);
+            es.execute(allGuests[i]);
+        }
+
+        es.shutdown();
+        boolean success = es.awaitTermination(2, TimeUnit.MINUTES);
+
+        long end = System.currentTimeMillis();
+
+        System.out.println("--------------------");
+        if (!success)
+            System.out.println("Not all guests finished viewing before 2m timeout.");
+        else
+            System.out.println("All guests are now finished viewing the vase!");
+
+        double sum = 0;
+        for (int i = 0; i < N; i++) {
+            sum += allGuests[i].getTimesVisited();
+            System.out.printf("Guest %d viewed the vase %d times.\n", i, allGuests[i].getTimesVisited());
+        }
+        System.out.printf("Average number of views per guest: %.2f.\n", sum / N);
+        System.out.printf("Execution time: %d\n", (end - start));
     }
 }
---
+++ Assignment2/P2-CrystalVase/GuestThread.java
@@ -0,0 +1,110 @@
+import java.util.Queue;
+
+public class GuestThread extends Thread {
+
+    private static int ID_COUNTER = 0;
+    private int id = 0;
+
+    private final int DELAY_TO_QUEUE = 10;
+    private final int MAX_TIME_IN = 15;
+    private final double RUN_AGAIN_THRESHOLD = .10;
+
+    private int timesVisited = 0;
+    private boolean runAgain = true;
+    private Queue<GuestThread> vaseRoom;
+
+    public GuestThread(Queue<GuestThread> vaseRoom) {
+        this.vaseRoom = vaseRoom;
+        this.id = ID_COUNTER++;
+    }
+
+    public int id() {
+        return id;
+    }
+
+    public int getTimesVisited() {
+        return timesVisited;
+    }
+
+    /*
+     * 
+     * Choose a random offset for when they will next enter the room
+     * Wait that long
+     * Join the queue to enter the room and wait
+     * When predecessor finished, will be notified to wake up
+     * 
+     * When entering the room, wait a random amount of time in the room
+     * When leaving, notify the next guest thread waiting
+     * Randomly decide to queue up again or to leave
+     * 
+     */
+
+    @Override
+    public void run() {
+
+        while (runAgain) {
+            // Wait some amount of time before adding to the queue
+            int wait = (int) (Math.random() * DELAY_TO_QUEUE);
+
+            try {
+                sleep(wait);
+            } catch (InterruptedException e) {
+                e.printStackTrace();
+            }
+
+            // Adds itself to the queue and checks if they are at the front
+            boolean shouldWait = true;
+            synchronized (vaseRoom) {
+                shouldWait = !vaseRoom.isEmpty();
+                vaseRoom.add(this);
+            }
+
+            // System.out.printf("Guest %d queueing up (new queue size = %d)\n", id,
+            // vaseRoom.size());
+
+            // If they aren't at the front, have them wait to be notified
+            synchronized (this) {
+                if (shouldWait) {
+                    try {
+                        wait();
+                    } catch (InterruptedException e) {
+                        e.printStackTrace();
+                    }
+                }
+            }
+
+            // System.out.printf("Guest %d entering room\n", id);
+
+            // Visit the room for a random amount of time
+            timesVisited++;
+            int stay = (int) (Math.random() * MAX_TIME_IN);
+
+            try {
+                sleep(stay);
+            } catch (InterruptedException e) {
+                e.printStackTrace();
+            }
+
+            GuestThread nextGuest;
+            synchronized (vaseRoom) {
+                // System.out.printf("Guest %d leaving room\n", id);
+                // Remove itself from the queue
+                vaseRoom.poll();
+                nextGuest = vaseRoom.peek();
+            }
+
+            // Notify the next guest in line, if there is one and they are waiting
+            if (nextGuest != null) {
+                synchronized (nextGuest) {
+                    nextGuest.notify();
+                    // System.out.printf("Notifying guest %d\n", nextGuest.id);
+                }
+            }
+
+            // Decide whether to re-queue after a delay
+            runAgain = Math.random() < RUN_AGAIN_THRESHOLD;
+
+            // System.out.printf("Guest %d requeing ? %b\n", id, runAgain);
+        }
+    }
+}
